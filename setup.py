2c68559c81e060d90363a0eac831428968823c98
karmakaragradwip02 <karmakaragradwip02@gmail.com>
2024-10-03 23:47:20 +0530

    filesimported

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'VNP_Detector',
+    version= '0.0.0',
+    author= 'Karmakaragradwip02',
+    author_email= 'karmakaragradwip02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
